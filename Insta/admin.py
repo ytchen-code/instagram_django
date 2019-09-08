7b028b5020ac7fa32a5465d2b3c5d3b13da08d2f
Yuting Chen <c.sunflower1983@gmail.com>
2019-09-07 21:53:00 -0700

    new upload

---
+++ Insta/admin.py
@@ -0,0 +1,13 @@
+from django.contrib import admin
+
+from Insta.models import Post, InstaUser, Like, UserConnection
+
+# Register your models here.
+admin.site.register(Post)
+admin.site.register(InstaUser)
+admin.site.register(Like)
+admin.site.register(UserConnection)
+
+
+
+
---
+++ Insta/urls.py
@@ -0,0 +1,32 @@
+"""InstaJZ URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/2.2/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path, include
+
+from Insta.views import (HelloWorld, PostDetailView, UserDetailView, PostsView, PostCreateView, 
+PostUpdateView, PostDeleteView, addLike)
+
+
+urlpatterns = [
+    path('helloworld/', HelloWorld.as_view(), name='helloworld'),
+    path('', PostsView.as_view(), name='posts'),
+    path('post/<int:pk>', PostDetailView.as_view(), name='post_detail'),
+    path('post/new/', PostCreateView.as_view(), name='make_post'),
+    path('post/update/<int:pk>', PostUpdateView.as_view(), name='post_update'),
+    path('post/delete/<int:pk>', PostDeleteView.as_view(), name='post_delete'),
+    path('insta/like', addLike, name='addLike'),
+    path('user/<int:pk>', UserDetailView.as_view(), name='user_detail'),
+]
